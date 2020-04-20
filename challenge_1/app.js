b2ec876472e9a77908edc00bd04e0c5f1d12052e
Nathaniel Torrez <mr.realestatenate@gmail.com>
2020-04-20 12:14:41 -0700

    updated the table to show boxes

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,1 +1,44 @@
-console.log('hello world')
+// define the board
+let board = [[0, 0, 0],
+             [0, 0, 0],
+             [0, 0, 0]]
+
+//HANDLES THE RENDERING OF X OR O
+
+let playerTurn = true;
+
+let toggleXorO = () => {
+  playerTurn = !playerTurn
+}
+
+let addXorO = (event) => {
+
+  if (playerTurn) {
+    var symbol = 'X'
+  } else {
+    var symbol = 'O'
+  }
+
+  let cellToChange = event.target;
+  cellToChange.innerHTML = symbol;
+
+  toggleXorO()
+}
+
+
+// apply an event listner to all items of the table
+
+let clickHandler = (event) => {
+
+  let cells = document.getElementById("table").querySelectorAll("td");
+
+  for (var i = 0; i < cells.length; i++) {
+
+    let currentCell = cells[i]
+    // TODO: change this function to something useful
+    currentCell.addEventListener("click", addXorO)
+  }
+};
+clickHandler()
+
+
